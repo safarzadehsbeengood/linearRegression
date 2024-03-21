88a2bb399da2a76692d48a60199a8432bcbf6df6
Ryan Safarzadeh <safarzadehryan@gmail.com>
2024-03-21 15:39:27 -0700

    added instructions, memory cleanup, loss tracker, lr

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 var cnv;
 let x_vals = [0.5];
 let y_vals = [0.5];
-let m, b;
-// learning rate
+let m, b, loss_val;
 const lr = 0.1;
-// optimizer
 const opt = tf.train.sgd(lr);
 
 function setup() {
-  cnv = createCanvas(windowWidth-100, windowHeight-100);
-  cnv.position((windowWidth-width)/2, (windowHeight-height)/2);
+  cnv = createCanvas(windowWidth - 100, windowHeight - 100);
+  cnv.position((windowWidth - width) / 2, (windowHeight - height) / 2);
   m = tf.variable(tf.scalar(random(1)));
   b = tf.variable(tf.scalar(random(1)));
-  
 }
 
 function loss(pred, labels) {
   // take predictions, sub difference from true values, square them, and take the mean
   const loss = pred.sub(labels).square().mean();
-  // loss.print();
+  loss_val = loss.dataSync()[0];
   return loss;
 }
 
@@ -30,13 +27,13 @@ function predict(x) {
 }
 
 function draw() {
-  let loss_val;
-  if (x_vals.length > 0) {
-  // optimize
-    const ty = tf.tensor1d(y_vals);
-    opt.minimize(() => loss(predict(x_vals), ty));
-    loss_val = loss(predict(x_vals), ty);
-  }
+  tf.tidy(() => {
+    if (x_vals.length > 0) {
+      // optimize
+      const ty = tf.tensor1d(y_vals);
+      opt.minimize(() => loss(predict(x_vals), ty));
+    }
+  });
   background(0);
   stroke(255);
   strokeWeight(6);
@@ -45,24 +42,36 @@ function draw() {
   }
   strokeWeight(4);
   stroke(color(70, 230, 70));
-  const xd = [0, 1];
-  const yd = predict(xd).dataSync();
-  let x1 = xd[0] * width;
-  let y1 = map(yd[0], 0, 1, height, 0);
-  let x2 = xd[1] * width;
-  let y2 = map(yd[1], 0, 1, height, 0);
-  line(x1, y1, x2, y2);
-  fill(0);
-  noStroke();
+  tf.tidy(() => {
+    const xd = [0, 1];
+    const yd = predict(xd);
+    const yvals = yd.dataSync();
+    let x1 = xd[0] * width;
+    let y1 = map(yvals[0], 0, 1, height, 0);
+    let x2 = xd[1] * width;
+    let y2 = map(yvals[1], 0, 1, height, 0);
+    line(x1, y1, x2, y2);
+  });
+  console.log(tf.memory().numTensors);
   fill(255);
-  text("Loss", 30, 25)
-  text(loss_val.dataSync()[0].toFixed(6), 30, 40);
+  noStroke();
+  text("Loss", 30, 25);
+  text(loss_val.toFixed(6), 30, 40);
+  text(`lr: ${lr.toFixed(2)}`, width - 60, 40);
   stroke(255);
   noFill();
   rect(0, 0, width, height);
+  fill(100);
+  point(mouseX, mouseY, 20);
 }
 
 function mousePressed() {
-  x_vals.push(map(mouseX, 0, width, 0, 1));
-  y_vals.push(map(mouseY, 0, height, 1, 0));
+  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+    x_vals.push(map(mouseX, 0, width, 0, 1));
+    y_vals.push(map(mouseY, 0, height, 1, 0));
+  }
 }
+
+window.onresize = () => {
+  resizeCanvas(windowWidth - 100, windowHeight - 100);
+};
